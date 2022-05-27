24bd401751281fc706a4d5293de68aae6cff9fe1
dhs111 <netus@seed.net.tw>
2022-05-27 16:08:44 +0800

    1608
    1608

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
+//const express = require('express')
+//const path = require('path')
+//const PORT = process.env.PORT || 5000
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -11,4 +11,13 @@ express()
 		res.sendFile(__dirname + '/bc.jpg');          
 	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
+*/
+var app = express();
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index2'))
+	app.get('/bc.jpg', function (req, res) {
+		res.sendFile(__dirname + '/bc.jpg');          
+	})
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- index.js.bak
+++ index.js.bak
@@ -2,15 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-	app.get('/bc.jpg', function (req, res) {
-		res.sendFile(__dirname + '/bc.jpg');          
-	})
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index2'))
-
+	.get('/bc.jpg', function (req, res) {
+		res.sendFile(__dirname + '/bc.jpg');          
+	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
